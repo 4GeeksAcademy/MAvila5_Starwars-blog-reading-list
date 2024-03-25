71d59c61300c394325811627908d16f31e6f9d6e
MAvila5 <61132417+MAvila5@users.noreply.github.com>
2024-03-25 02:44:01 +0000

    Attempt to fix index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './../styles/index.css';
+import '../styles/index.css';
 import App from './App';
 import { injectContext } from './store/appContext';
 
 const AppWithStore = injectContext(App);
+const container = document.getElementById('root');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <AppWithStore />
-    </React.StrictMode>
-);
+if (container !== null) {
+    const root = createRoot(container);
+    root.render(
+        <React.StrictMode>
+            <AppWithStore />
+        </React.StrictMode>
+    );
+} else {
+    console.error('Failed to find the root element');
+}
